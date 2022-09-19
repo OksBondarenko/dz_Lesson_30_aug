3efe80e0438829e2a09ac1985d0feb93f05a1875
Oksana bondarenko <roksanapopova@gmail.com>
2022-09-19 21:07:46 +0200

    Сформируйте трёхмерный массив

---
+++ Task60/Program.cs
@@ -0,0 +1,55 @@
+﻿//Сформируйте трёхмерный массив из случайных неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.WriteLine("Введите l: ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matr = FillMatrix(l, m, n, 10, 100);
+PrintMatrix(matr);
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+            Console.Write($"{matrix[i,j,k]} ({i}, {j}, {k}) ");
+            }
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,,] FillMatrix(int rowsCount, int columonsCount, int depthCount, int leftRange, int rightRange)
+{
+    int[,,] matrix = new int[rowsCount, columonsCount, depthCount];
+    int[] uniqValue = new int[90];
+    int value;
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrix.GetLength(2); k++)
+            {
+                do
+                    value = rand.Next(leftRange, rightRange);
+                    while (value == uniqValue[value - 10]);
+                    uniqValue[value - 10] = value;
+                    matrix[i, j, k] = value;
+            }
+        }
+    }
+    return matrix;
+}
+
